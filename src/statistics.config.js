3c85cf70766e43c9f71633084363c2deb317c1e2
melonglong <wangzilongnews@163.com>
2019-03-26 19:57:32 +0800

    server

---
+++ src/statistics.config.js
@@ -0,0 +1,3 @@
+export default {
+  'app_key' : '73bbb1684f9051e9a6eea98c1291b51b'   //请在此行填写统计平台获取的 app_kye
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,31 @@
 import fetch from '@system.fetch';
+import storage from"@system.storage";
 
 function requestHandle(params) {
     // console.log(fetch)
+    let sid;
+    storage.get({
+      key:"sid",
+      success: function(data) {
+        sid = data;
+      },
+      fail: function(data, code) {
+        console.log(`handling fail, code = ${code}`)
+      }
+    })
     return new Promise((resolve, reject) => {
       fetch.fetch({
         url: params.url,
+        header: {
+          cookie: `sid=${sid}`
+        },
         method: params.method,
         data: params.data || {},
         responseType: 'json',
       }).then(response => {
         const result = response.data
         const content = result.data
-        return content.code === 200 ? resolve(content) : resolve(content.message)
+        return  resolve(content)
       }).catch((error, code) => {
         console.log(`🐛 request fail, code = ${code}`)
         return reject(error)
