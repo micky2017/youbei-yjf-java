d857471ac8165b241f084157fc78b3fc440f5b39
melonglong <wangzilongnews@163.com>
2019-07-16 10:44:41 +0800

    JDE

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,16 @@ import fetch from '@system.fetch';
 import storage from"@system.storage";
 
 const  requestHandle = (params)  => {
-    // console.log(fetch)
-
+    // if(params.data){
+    //   params.data.testpackage = "test";
+    // }else{
+    //   params.url = params.url.indexOf('?') != -1 ? `${params.url}&testpackage=test` : `${params.url}?testpackage=test`
+    // }
+    // if(params.data){
+    //   params.data.environment = "duli";
+    // }else{
+    //   params.url = params.url.indexOf('?') != -1 ? `${params.url}&environment=duli` : `${params.url}?environment=duli`
+    // }
     let sid;
     return new Promise((resolve, reject) => {
       storage.get({
@@ -58,29 +66,31 @@ const  requestHandle = (params)  => {
   export default {
     post: function(url, params) {
       // params.environment = "test"
-      // params.environment = "duli"
+      // params.environment = "duli";
+      // params = Object.assign(params,{environment:'duli'})
       // params.environment = "yanzheng"
       // params.testpackage = 'test'
       return requestHandle({
         method: 'post',
-        url: `https://api.ubaycn.com/${url}`,
+        // url: `https://api.ubaycn.com/${url}`,
         // url: `https://test-api.ubaycn.com/${url}`,
         // url: `http://192.168.8.121:7010/${url}`,
-        // url: `http://192.168.1.101:7010/${url}`,
+        url: `http://192.168.1.101:7010/${url}`,
         data: params
       })
     },
     get: function(url, params) {
+      // params = Object.assign(params,{environment:'duli'})
       // params.environment = "test"
-      // params.environment = "duli"
+      // params.environment = "duli";
       // params.environment = "yanzheng"
       // params.testpackage = 'test'
       return requestHandle({
         method: 'get',
         // url: queryString(`http://192.168.8.121:7010/${url}`, params),
-        // url: queryString(`http://192.168.1.101:7010/${url}`, params),
+        url: queryString(`http://192.168.1.101:7010/${url}`, params),
         // url: queryString(`https://test-api.ubaycn.com/${url}`, params),
-        url: queryString(`https://api.ubaycn.com/${url}`, params),
+        // url: queryString(`https://api.ubaycn.com/${url}`, params),
         data:{},
       })
     }
